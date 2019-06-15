748ec0814c523068759895dd28bb8425a900e4af
jerassi <gjerassi@gmail.com>
2019-06-16 00:07:43 +0900

    Add intro2 page (xml)

--- app/src/main/java/com/example/lawlawtest/MainActivity.java
+++ app/src/main/java/com/example/lawlawtest/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lawlawtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Handler handler = new Handler(){
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                startActivity(new Intent(MainActivity.this, Intro1.class));
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 3000);
     }
 }
